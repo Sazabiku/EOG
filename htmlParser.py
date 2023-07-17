0b08bed0af7123ea32fd683f115f359dc3d063c2
Nikolai <solairesunnydesor@gmail.com>
2023-07-17 15:19:12 +0500

    Parsing added

--- htmlParser.py
+++ htmlParser.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
-import io
+
+class Dictlist(dict):
+    def __setitem__(self, key, value):
+        try:
+            self[key]
+        except KeyError:
+            super(Dictlist, self).__setitem__(key, [])
+        self[key].append(value)
 
 
 with open('downloads/Куприн_Вячеслав_Александрович_2.html', 'r', encoding='utf-8') as f:
@@ -17,5 +24,18 @@ with open('downloads/Куприн_Вячеслав_Александрович_2.
     
     with open ('htmltext.txt', 'w', encoding='utf-8') as txtFile:
         
-        for tag in soup.find_all('div'):
-            txtFile.write(f'{tag.text}')
+        
+        #"class": "grid_column_title"
+        
+        tags =[]
+        results=[]
+        
+        for tag in soup.find_all("div", class_="grid_column_title"):
+            tags.append(tag.text)
+            
+        for result in soup.find_all("div", class_="grid_column_result"):
+            results.append(result.text)
+            
+            
+        for key, value in zip(tags, results): 
+            txtFile.write('%s %s\n' % (key, value))
