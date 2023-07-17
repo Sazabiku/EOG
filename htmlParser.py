5fc9168a6bc635dec470044f02775cff94650a3d
Nikolai <solairesunnydesor@gmail.com>
2023-07-17 12:53:38 +0500

    Editing parser

--- htmlParser.py
+++ htmlParser.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
+import io
 
-with open('Куприн_Вячеслав_Александрович_2.html', 'r') as f:
+
+with open('downloads/Куприн_Вячеслав_Александрович_2.html', 'r', encoding='utf-8') as f:
 
     contents = f.read()
 
@@ -10,3 +12,16 @@ with open('Куприн_Вячеслав_Александрович_2.html', 'r'
 
     root_childs = [e.name for e in root.children if e.name is not None]
     print(root_childs)
+    
+    root_childs = [e.name for e in root.descendants if e.name is not None]
+    print(root_childs)
+    
+    #for tag in soup.find_all('div'):
+     #   print(f'{tag.text}')
+        
+    #html_text = soup.find_all('div')
+    
+    with open ('htmltext.txt', 'w', encoding='utf-8') as txtFile:
+        
+        for tag in soup.find_all('div'):
+            txtFile.write(f'{tag.text}')
