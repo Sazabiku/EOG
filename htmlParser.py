b0c23651cefde59c9192b45c8446b574e2233d9d
Nikolai <solairesunnydesor@gmail.com>
2023-07-17 12:25:31 +0500

    Editing HTML parser functions

--- htmlParser.py
+++ htmlParser.py
@@ -1,2 +1,12 @@
 from bs4 import BeautifulSoup
 
+with open('Куприн_Вячеслав_Александрович_2.html', 'r') as f:
+
+    contents = f.read()
+
+    soup = BeautifulSoup(contents, 'lxml')
+
+    root = soup.html
+
+    root_childs = [e.name for e in root.children if e.name is not None]
+    print(root_childs)
