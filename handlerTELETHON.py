72c7230c3bfe2ebf18d2f74425dab546e9b31992
Nikolai <solairesunnydesor@gmail.com>
2023-07-18 10:56:05 +0500

    Editing responces

--- handlerTELETHON.py
+++ handlerTELETHON.py
@@ -18,6 +18,8 @@ countries = ['Россия', 'Украина', 'Беларусь', 'Казахс
              'Армения', 'Африка', 'Франция', 'Мальта', 'Норвегия', 'Малайзия', 'Узбекистан', 'Таджикистан',
              'Нидерланды', 'ОАЭ', 'Румыния', 'Польша', 'Испания']
 
+socnetworks =['ВКонтакте', 'Instagram', 'Telegram', 'Пароли']
+
 
 #Пример данных для поиска, потом подгружать с БД/запроса (?)
 keys_search = ['Антонов Вячеслав Олегович', 'Антонов Георгий Олегович'] # 'Сидоров Вячеслав Олегович', 
@@ -40,12 +42,14 @@ async def greet_handler(event):
 
 
 #Получаем данные html ответа бота
-@client.on(events.NewMessage(chats=target_chat, pattern=keys_search[action_count]))
+@client.on(events.NewMessage(chats=target_chat))
 async def doc_handler(event):
     global action_count
-    if event.message.document:
-        await event.message.download_media()#Можно задать конкретный путь при запуске на VM
-    
+    if event.media:
+        await event.download_media()#Можно задать конкретный путь при запуске на VM
+    if 'ФИО' in event.text or 'Логин' in event.text or 'ИНН' in event.text or 'Адрес' in event.text or 'VIN' in event.text:
+        with open ('messages.txt', 'a', encoding='utf-8') as file:
+            file.write(event.text)
 
 #Работаем с query-ответами бота, выбираем страну для посика, проверяем, удачный ли ответ и продолжаем опрашивать бота
 @njit
@@ -88,6 +92,11 @@ async def greet_handler(event):
     if 'Вы можете прислать боту запросы в следующем формате:' in event.text:
         await event.reply(keys_search[action_count])
 
+
+@njit
+@client.on(events.NewMessage(chats=target_chat, pattern=r'Выберите направление поиска'))
+async def socnet_handler(event):
+    await event.message.click(socnetworks.index('Вконтакте'))
 
 #Запускаем в работу
 if __name__ == "__main__":
