754ff6f8b3175b35a4f207aecab09d8ffc6f2473
Nikolai <solairesunnydesor@gmail.com>
2023-07-26 10:53:15 +0500

    Start of server-like operations implementation with responcer.py.

---
+++ responcer.py
@@ -0,0 +1,42 @@
+import grpc
+import API_pb2
+import API_pb2_grpc
+import handler
+
+
+import time
+from concurrent import futures
+
+class EOGServicer(API_pb2_grpc.EOGServicer):
+    def GET(self, request, context):
+        return super().GET(request, context)
+    
+    
+    def GET_BISTREAM(self, request_iterator, context):
+        return super().GET_BISTREAM(request_iterator, context)
+    
+    def GET_RESSTREAM(self, request, context):
+        return super().GET_RESSTREAM(request, context)
+    
+    def LOGIN(self, request, context):
+        return super().LOGIN(request, context)
+    
+    def SEND_CODE(self, request, context):
+        return super().SEND_CODE(request, context)
+    
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    API_pb2_grpc.add_EOGServicer_to_server(EOGServicer(), server)
+    server.add_insecure_port('localhost:3128')
+    server.start()
+    server.wait_for_termination(
+    )
+    
+def get_info_from_handler():
+    pass
+
+def start_handler():
+    handler.main()
+    
+if __name__ == "__main__":
+    serve();
