0017e60adb742b4a6e6c01f5fc0950b4b6506245
Nikolai <nik.baturov@mail.ru>
2023-08-31 10:27:29 +0500

    Parser finished

--- htmlParser.py
+++ htmlParser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import os, errno
-
+#need to pip install lxml
 
 
 def silentremove(filename):
@@ -20,15 +20,18 @@ def parse_html (input_file, output_file):
         soup = BeautifulSoup(contents, 'lxml')
 
         root = soup.html
+
+        print('complete')
+        print(root)
         
-        with open (output_file, 'w', encoding='utf-8') as outf:
+        with open (output_file, 'a', encoding='utf-8') as outf:
             tags =[]
             results=[]
             
-            for tag in soup.find_all("div", class_="grid_column_title"):
+            for tag in root.find_all("div", class_="grid_column_title"):
                 tags.append(tag.text)
                 
-            for result in soup.find_all("div", class_="grid_column_result"):
+            for result in root.find_all("div", class_="grid_column_result"):
                 results.append(result.text)
                 
                 
@@ -36,8 +39,16 @@ def parse_html (input_file, output_file):
                 outf.write('%s %s\n' % (key, value))
 
 
-def read_txt (input_file, output_array):
+def read_txt (input_file):
     with open (input_file, 'r', encoding='utf-8') as f:
         txt_data = f.read()
         output_array = txt_data.split('\n')
     return output_array
+
+
+if __name__ == '__main__':
+    print('Hello')
+    parse_html('Антонов Вячеслав Игоревич.html', 'Антонов Вячеслав Игоревич.txt')
+    text = []
+    text = read_txt('Антонов Вячеслав Игоревич.txt')
+    print(text)
